c94576fd4109ec6b308318a0f4dc61fbcd5bdcbe
Automation-qa-ks <mailtosuneethak@gmail.com>
2019-12-11 11:23:55 -0800

    Initial solution for finding unique pair

---
+++ JavaCollections/src/main/java/Hashset_findUniquepair.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Hashset_findUniquepair {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int size=Integer.parseInt(sc.nextLine());
+        HashSet <String> all=new HashSet<String>();
+
+        for(int i=0;i<size;i++)
+        {
+
+            String input=sc.nextLine();
+            all.add(sc.nextLine());
+
+        }
+        System.out.println(all.size());
+
+
+
+    }
+
+
+}
